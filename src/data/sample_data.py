10455be2163498d80ad840d323b1cd649782a3ad
MrCreeperman147 <clement.ogbeiwi@gmail.com>
2025-10-05 21:44:05 -0400

    added radar image

--- src/data/sample_data.py
+++
@@ -1,7 +0,0 @@
-def get_sample_data():
-    # Sample data for testing the map generation
-    return [
-        {"name": "Location A", "coordinates": (37.7749, -122.4194)},  # San Francisco
-        {"name": "Location B", "coordinates": (34.0522, -118.2437)},  # Los Angeles
-        {"name": "Location C", "coordinates": (40.7128, -74.0060)},   # New York
-    ]
